56ff375f6ff948d9bd89969025a33d9a29ba0008
Erica Odeje <72512702+EricaOdeje@users.noreply.github.com>
2023-11-26 12:21:30 +0000

    Update bookItem.js

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -9,10 +9,11 @@ function BookItem(props) {
                 {/*I used props.mybook.title to pass data from one component to another.*/}
                 <Card.Body>{props.mybook.title}</Card.Body>
             </Card>
+    {/* Displaying the cover image of the book using the src from props.mybook.cover. */}
             <img src={props.mybook.cover}></img>
             <p>{props.mybook.author}</p>
         	
-            {/*  This link will edit a FINISH COMMENT */}
+           {/* This Link component creates a hyperlink to the edit page for the specific book using the book's _id. */}
             <Link to={"/edit/" + props.mybook._id} className='btn btn-primary'>Edit</Link>
         </div>
     );
